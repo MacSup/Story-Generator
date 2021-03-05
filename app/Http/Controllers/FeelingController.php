1c8cd5af73b5dbee18cd3ff17323929871ca8200
Amaury Guillermin <amauryguillermin@gmail.com>
2021-03-05 18:36:29 +0100

    Models + controllers for 'problem' part

---
+++ app/Http/Controllers/FeelingController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feeling;
+use Illuminate\Http\Request;
+
+class FeelingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Feeling  $feeling
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Feeling $feeling)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Feeling  $feeling
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Feeling $feeling)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Feeling  $feeling
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Feeling $feeling)
+    {
+        //
+    }
+}
